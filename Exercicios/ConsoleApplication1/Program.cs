77ee176501480f3fd886aa157178946456446c32
CarolineCosta <carolineetiene@hotmail.com>
2017-05-01 19:25:57 -0300

    adição de uma sub- rotina

--- Exercicios/ConsoleApplication1/Program.cs
+++ Exercicios/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             int N;
-            string modelo,tipo,tipod;
+            string modelo, sub;
             double km,mot;
             
             Console.WriteLine("Informe a quantidade de carros no galpão");
@@ -28,35 +28,64 @@ namespace ConsoleApplication1
                 Console.WriteLine("Informe a potência do motor");
                 mot = Double.Parse(Console.ReadLine());
 
-                if (km <= 5000)
-                {
-                    tipo = "novo";
-                }
-                else if (km > 5000 && km <= 30000)
-                {
-                    tipo = "seminovo";
-                }
-                else
-                {
-                    tipo = "velho";
-                }              
-                
-                if (mot > 200)
-                {
-                    tipod = "potente";
-                }
-                else if (mot >= 120 && mot <= 200)
-                {
-                    tipod = "forte";
-                }
-                else
-                {
-                    tipod = "popular";
-                }               
 
-                Console.WriteLine("{0} - {1} {2}", modelo, tipo, tipod);
+                sub = Classificar (modelo, km, mot );
+
+                Console.WriteLine("{0} - {1}", modelo,sub);
+            
             }
 
         }
+
+        
+        static string Classificar(string mod, double km, double mot)
+        {
+
+            string tipo, tipod;
+
+
+            if (km <= 5000)
+            {
+               tipo = "novo";
+            }
+
+            else if (km > 5000 && km <= 30000)
+            {
+               tipo = "seminovo";
+            }
+
+            else
+            {
+               tipo = "velho";
+            }              
+                
+            if (mot > 200)
+            {
+               tipod = "potente";
+            }
+
+            else if (mot >= 120 && mot <= 200)
+            {
+               tipod = "forte";
+            }
+
+            else
+            {
+               tipod = "popular";
+            }               
+
+            return tipo + " " + tipod;
+
+            
+
+        }
+
+
+
+
+
+
+
+        
     }
 }
