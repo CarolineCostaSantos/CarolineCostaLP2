3c71feeb1c863a11e71e45b05fd3c22e8bec7a8e
CarolineCostaSantos <carolineetiene@hotmail.com>
2017-08-02 14:43:20 -0300

    Convertendo Temperatura Celsius

---
+++ 2C/Temperatura/Temperatura/TemperaturaCelsius.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura
+{
+    class TemperaturaCelsius
+    {
+        public double celsius;
+
+
+            public double ParaFahrenheit()
+            {
+                return (celsius * 9 / 5) + 32;
+
+            }
+
+        public double ParaKelvin()
+            {
+               return celsius + 273;
+            }
+
+
+    }
+}
