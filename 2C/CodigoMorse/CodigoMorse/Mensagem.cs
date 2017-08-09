14580299f4d9ccdc11ba6fab32ce26b7d6d96c54
Caroline Costa <carolineetiene@hotmail.com>
2017-08-09 15:58:56 -0300

    "Corpo" do códico morse

---
+++ 2C/CodigoMorse/CodigoMorse/Mensagem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodigoMorse
+{
+    class Mensagem
+    {
+        public string txt;
+
+        private string[] tab = new string[]   {
+
+        ".-", "-...","-.-.","-..",".","..-.","--.","....","..",
+        ".---","-.-",".-..","--","-.","---",".--.","--.-",".-.",
+        "...","-","..-","...-",".--","-..-","-.--","--.."};
+
+
+        public string Codificar()
+        {
+            txt.length
+            return  ;
+        }
+
+        public string Decodificar()
+        {
+
+            return;
+        }
+
+        public ____ Transmitir()
+        {
+
+            return;
+        }
+    }
+}
