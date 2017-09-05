9c0661be1fa03f4bf35a42371cab438e8c02d166
Caroline Costa <carolineetiene@hotmail.com>
2017-09-04 21:39:41 -0300

    feito todas as conversoes de Metro e Quilômetro

--- 2C/ConversorMedidas/ConversorMedidas/ConversorDistancia.cs
+++ 2C/ConversorMedidas/ConversorMedidas/ConversorDistancia.cs
@@ -15,19 +15,69 @@ namespace ConversorMedidas
             return x / 1000;
         }
 
-        public static double MparaHm()
+        public static double KmparaM()
+        {
+            return x * 1000;
+        }
+
+        public static double MparaCm()
+        {
+            return x * 100;
+        }
+
+        public static double CmparaM()
         {
             return x / 100;
         }
 
-        public static double MparaDam()
+        public static double MparaPoleg()
         {
-            return x / 10;
+            return x * 39.3701;
         }
 
-        public static double MparaDm()
+        public static double PolegparaM()
         {
-            return x * 10;
+            return x * 0.0254;
+        }
+
+        public static double MparaPes()
+        {
+            return x * 3.28084;
+        }
+
+        public static double PesparaM()
+        {
+            return x * 0.3048;
+        }
+
+        public static double KmparaCm()
+        {
+            return x * 100000;
+        }
+
+        public static double CmparaKm()
+        {
+            return x / 100000;
+        }
+
+        public static double KmparaPoleg()
+        {
+            return x * 39370.1;
+        }
+
+        public static double PolegparaKm()
+        {
+            return x * 0.0000254;
+        }
+
+        public static double KmparaPes()
+        {
+            return x * 3280.84;
+        }
+
+        public static double PesparaKm()
+        {
+            return x * 0.0003048;
         }
     }
 }
