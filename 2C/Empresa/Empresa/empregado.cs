3998a44ece5f42390b167c18a62db2dc50deef90
Caroline Costa <carolineetiene@hotmail.com>
2017-09-06 18:02:51 -0300

    Exercicio sonre a matéria Herança

---
+++ 2C/Empresa/Empresa/empregado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Empresa
+{   
+    class Empregado: Funcionario
+    {
+       private  int dEntrada;
+       public Gerente gerente {get; set;}
+
+
+       public int DEntrada
+       {
+           get { return dEntrada; }
+       }
+
+        public Empregado( int Data)
+       {
+           Data = dEntrada;
+       }
+
+    }
+}
