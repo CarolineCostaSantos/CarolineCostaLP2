8d1cec27b76d0abae09ac241561348360329c094
Caroline Costa <carolineetiene@hotmail.com>
2017-09-22 20:16:46 -0300

    Exercício "Conversor de Medidas" finalizado

--- 2C/ConversorMedidas/ConversorMedidas/Program.cs
+++ 2C/ConversorMedidas/ConversorMedidas/Program.cs
@@ -11,7 +11,7 @@ namespace ConversorMedidas
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Qual Conversor deseja usar?: 1- Distância | 2- Temperatura | 3- Massa");
+            Console.WriteLine("1- Distância | 2- Temperatura | 3- Massa");
             int r = int.Parse(Console.ReadLine());
 
             if (r == 1)
@@ -19,17 +19,17 @@ namespace ConversorMedidas
                 Console.WriteLine("Digite a distância");
                 double x = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Qual conversão deseja fazer?: 1- M para Km | 2- Km para M");
+                Console.WriteLine("1- M para Km | 2- Km para M");
                 int c = int.Parse(Console.ReadLine());
 
                 if (c == 1)
                 {
-                    Console.WriteLine(ConversorDistancia.MparaKm(x));
+                    Console.WriteLine("{0} m é equivalente a {1} km", x, ConversorDistancia.MparaKm(x));
                 }
 
                 else if (c == 2)
                 {
-                    Console.WriteLine(ConversorDistancia.KmparaM(x));
+                    Console.WriteLine("{0} km é equivalente a {1} m", x, ConversorDistancia.KmparaM(x));
                 }
             }
 
@@ -38,17 +38,17 @@ namespace ConversorMedidas
                 Console.WriteLine("Digite a temperatura");
                 double x = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Qual conversão deseja fazer?: 1- C para K | 2- K para C");
+                Console.WriteLine("1- C para K | 2- K para C");
                 int c = int.Parse(Console.ReadLine());
 
                 if (c == 1)
                 {
-                    Console.WriteLine(ConversorTemperatura.CparaK(x));
+                    Console.WriteLine("{0} °c é equivalente a {0} k", x, ConversorTemperatura.CparaK(x));
                 }
 
                 else if (c == 2)
                 {
-                    Console.WriteLine(ConversorTemperatura.KparaC(x));
+                    Console.WriteLine("{0} k é equivalente a {1} °c", x, ConversorTemperatura.KparaC(x));
                 }
             }
 
@@ -57,17 +57,17 @@ namespace ConversorMedidas
                 Console.WriteLine("Digite a massa");
                 double x = double.Parse(Console.ReadLine());
 
-                Console.WriteLine("Qual conversão deseja fazer?: 1- G para Kg | 2- Kg para G");
+                Console.WriteLine("1- G para Kg | 2- Kg para G");
                 int c = int.Parse(Console.ReadLine());
 
                 if (c == 1)
                 {
-                    Console.WriteLine(ConversorMassa.GparaKg(x));
+                    Console.WriteLine("{0} g é equivalente a {1} kg", x,  ConversorMassa.GparaKg(x));
                 }
 
                 else if (c == 2)
                 {
-                    Console.WriteLine(ConversorMassa.KgparaG(x));
+                    Console.WriteLine("{0} kg é equivalente a {1} g", x, ConversorMassa.KgparaG(x));
                 }
 
             }
