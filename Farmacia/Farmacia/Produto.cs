f9804dc03866abf4221b14dd341a27ba151ae5c3
caroline <carolineetiene@hotmail.com>
2018-01-24 15:54:56 +0000

    Iniciação de um menu com a opção "Cadastrar produto"; if de escolha da primeira opção; criação da classe produto com o método "Cadastrar"

---
+++ Farmacia/Farmacia/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacia
+{
+    class Produto
+    {
+        private string nome;
+        public string Nome { 
+            get { return nome; }
+        }
+
+        private string marca;
+        public string Marca {
+            get { return marca; }
+        }
+
+        public string Validade { get; set; }
+        public double Preco { get; set; }
+
+
+        public void Cadastro()
+        {
+            Console.WriteLine("Nome do produto: ");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Marca: ");
+            marca = Console.ReadLine();
+
+            Console.WriteLine("Validade: ");
+            Validade = Console.ReadLine();
+
+            Console.WriteLine("Preço: ");
+            Preco = double.Parse(Console.ReadLine());
+
+            SqlConnection conexao = new SqlConnection("Data Source=(localdb)lptrab;Initial Catalog=Farmacia;Integrated Security=SSPI;");
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = conexao;
+            cmd.CommandText = @"INSERT
+                                INTO Produto(nome, marca, validade, preco)
+                                Values (@nome, @marca, @validade, @preco);";
+
+            cmd.Parameters.AddWithValue("nome", nome);
+            cmd.Parameters.AddWithValue("marca", marca);
+            cmd.Parameters.AddWithValue("validade", Validade);
+            cmd.Parameters.AddWithValue("preco", Preco);
+
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+            cmd.Connection.Close();
+
+            Console.WriteLine("Produto cadastrado!");
+
+        }
+
+    }
+}
---
+++ Farmacia/Farmacia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farmacia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1 - Cadastrar produto\n 2 -");
+            int opcoes = int.Parse(Console.ReadLine());
+
+            if(opcoes == 1)
+            {
+                Produto produto = new Produto();
+                produto.Cadastro();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
